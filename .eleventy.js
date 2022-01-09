d9077aa71fdfe6faa1ad5293e4646b413f3416a0
widal001 <williamdaly422@gmail.com>
2022-01-09 18:08:29 -0500

    feat: adds _data/ directory and makes layout templates dynamic
    Uses ninjucks to dynamically populate the layout templates with data 
    from the frontmatter and the following files in the _data/ directory:
    - site.json
    - navigation.json

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = (config) => {
   config.addPassthroughCopy("./src/assets/");
   config.addPassthroughCopy("./src/js/");
+  config.addShortcode("year", () => `${new Date().getFullYear()}`);
   return {
     markdownTemplateEngine: "njk",
     dataTemplateEngine: "njk",
